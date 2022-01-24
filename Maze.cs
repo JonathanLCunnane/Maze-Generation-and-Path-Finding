da6123b146dced88a96ff0d8f465d66daea4d673
jonathan cunnane <jonathan.cunnane@gmail.com>
2022-01-24 22:21:19 +0000

    Created Maze and MazeNode, as well as basic maze initialisation.

---
+++ Maze.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// 
+/// </summary>
+
+
+namespace MazeDemonstration
+{
+    class Maze
+    {
+        // Maze Initialisation
+        int[] dimensions { get; }
+        MazeNode[,] nodes { get; set; }
+        Maze()
+        {
+            dimensions = new int[2] { 20, 20 };
+            nodes = InitialMaze(20, 20);
+        }
+        Maze(int x, int y)
+        {
+            dimensions = new int[2] { x, y };
+            nodes = InitialMaze(x, y);
+        }
+        MazeNode[,] InitialMaze(int x, int y)
+        {
+            // When creating a new maze, we will create a blank square with a border of walls all around it
+            nodes = new MazeNode[x,y];
+            for (int currX = 0; currX < x; currX++)
+            {
+                nodes[currX, 0].North = true;
+                nodes[currX, y - 1].South = true;
+            }
+            for (int currY = 0; currY < y; currY++)
+            {
+                nodes[0, currY].West = true;
+                nodes[x - 1, currY].East = true;
+            }
+            return nodes;
+        }
+    }
+}
---
+++ MazeNode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Each Maze Node will have a simple structure:
+/// Each of the Cardinal Directions will be used to represent if there is a wall in that direction
+/// True - Wall is in that direction
+/// False - Wall is not in that direction (i.e. Path is in that direction)
+/// e.g. North = True, East = False, South = True, West = True
+/// this will, when displayed, look like a C shape:
+///  _
+/// |
+///  ‾
+/// </summary>
+
+namespace MazeDemonstration
+{
+    class MazeNode
+    {
+        public bool North { get; set; }
+        public bool East { get; set; }
+        public bool South { get; set; }
+        public bool West { get; set; }
+        MazeNode()
+        {
+            North = false;
+            East = false;
+            South = false;
+            West = false;
+        }
+        MazeNode(bool north, bool east, bool south, bool west)
+        {
+            North = north;
+            East = east;
+            South = south;
+            West = west;
+        }
+    }
+}
