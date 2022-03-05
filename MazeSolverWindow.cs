33242725aec04e5d8536dda69caa042d1ebaa727
jonathan cunnane <jonathan.cunnane@gmail.com>
2022-03-05 18:52:46 +0000

    Added renderability for the start and finish points in the maze solver window.

--- MazeSolverWindow.cs
+++ MazeSolverWindow.cs
@@ -14,24 +14,29 @@ namespace MazeDemonstration
     {
         Maze maze { get; set; }
         PictureBox picturebox { get; set; }
+        Bitmap currBitmap { get; set; }
+        Brush bgBrush = new SolidBrush(DefaultBackColor);
         public MazeSolverWindow(Maze _maze, PictureBox _picturebox)
         {
             // Get previous maze and bitmap
             maze = _maze;
             picturebox = _picturebox;
+            currBitmap = (Bitmap)_picturebox.Image;
             // Initialise Component
             InitializeComponent();
             // Set Tags to default values
             startPointLabel.Tag = new Point(0, 0);
-            finishPointLabel.Tag = new Point(maze.dimensions[0], maze.dimensions[1]);
+            finishPointLabel.Tag = new Point(maze.dimensions[0] - 1, maze.dimensions[1] - 1);
             // Set up the current window with the maze not yet solved and other information.
             mazePictureBox.Size = picturebox.Size;
             mazePictureBox.Image = picturebox.Image;
 
-            dimensionsLabel.Text = $"Dimensions ({maze.dimensions[0]}, {maze.dimensions[1]})";
-            finishPointLabel.Text = $"Finish ({maze.dimensions[0]}, {maze.dimensions[1]})";
+            dimensionsLabel.Text = $"Dimensions - ({maze.dimensions[0]}, {maze.dimensions[1]})";
+            finishPointLabel.Text = $"Finish - ({maze.dimensions[0] - 1}, {maze.dimensions[1] - 1})";
 
-
+            // Draw Initial start and finish
+            mazePictureBox.Image = MazeSolving.changeStartPoint(currBitmap, null, (Point)startPointLabel.Tag, bgBrush);
+            mazePictureBox.Image = MazeSolving.changeFinishPoint(currBitmap, null, (Point)finishPointLabel.Tag, bgBrush);
         }
 
         private void exitSolver_Click(object sender, EventArgs e)
@@ -41,31 +46,37 @@ namespace MazeDemonstration
 
         private void setStart_Click(object sender, EventArgs e)
         {
-            Point max = new Point(maze.dimensions[0], maze.dimensions[1]);
+            Point max = new Point(maze.dimensions[0] - 1, maze.dimensions[1] - 1);
             StartPointDialogue startPointDiag = new StartPointDialogue(((Point)startPointLabel.Tag).x, ((Point)startPointLabel.Tag).y, max);
             DialogResult startPointDiagResult = startPointDiag.ShowDialog();
 
             if (startPointDiagResult == DialogResult.OK)
             {
+                Point prev = new Point(((Point)startPointLabel.Tag).x, ((Point)startPointLabel.Tag).y);
                 // Change start point label.
                 startPointLabel.Text = $"Start - ({startPointDiag.x}, {startPointDiag.y})";
-                startPointLabel.Tag = new Point(startPointDiag.x, startPointDiag.y);
+                Point next = new Point(startPointDiag.x, startPointDiag.y);
+                startPointLabel.Tag = next;
                 // Set location of start on the maze.
+                mazePictureBox.Image = MazeSolving.changeStartPoint(currBitmap, prev, next, bgBrush);
             }
         }
 
         private void setFinish_Click(object sender, EventArgs e)
         {
-            Point max = new Point(maze.dimensions[0], maze.dimensions[1]);
+            Point max = new Point(maze.dimensions[0] - 1, maze.dimensions[1] - 1);
             FinishPointDialogue finishPointDiag = new FinishPointDialogue(((Point)finishPointLabel.Tag).x, ((Point)finishPointLabel.Tag).y, max);
             DialogResult finishPointDiagResult = finishPointDiag.ShowDialog();
 
             if (finishPointDiagResult == DialogResult.OK)
             {
+                Point prev = new Point(((Point)finishPointLabel.Tag).x, ((Point)finishPointLabel.Tag).y);
                 // Change start point label.
-                startPointLabel.Text = $"Start - ({finishPointDiag.x}, {finishPointDiag.y})";
-                startPointLabel.Tag = new Point(finishPointDiag.x, finishPointDiag.y);
+                finishPointLabel.Text = $"Finish - ({finishPointDiag.x}, {finishPointDiag.y})";
+                Point next = new Point(finishPointDiag.x, finishPointDiag.y);
+                finishPointLabel.Tag = next;
                 // Set location of start on the maze.
+                mazePictureBox.Image = MazeSolving.changeFinishPoint(currBitmap, prev, next, bgBrush);
             }
         }
     }
---
+++ MazeSolving.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MazeDemonstration
+{
+    public static class MazeSolving
+    {
+        static Brush startColour = new SolidBrush(Color.FromArgb(0, 176, 80));
+        static Brush finishColour = new SolidBrush(Color.FromArgb(255, 0, 0));
+        public static Bitmap changeStartPoint(Bitmap mazeBitmap, Point oldStart, Point newStart, Brush bgColour)
+        {
+            using (Graphics bitmapGraphics = Graphics.FromImage(mazeBitmap))
+            {
+                int rectangleX;
+                int rectangleY;
+                int rectangleWidth = Consts.pixelsPerDimension - Consts.wallThickness;
+                int rectangleHeight = Consts.pixelsPerDimension - Consts.wallThickness;
+                // Remove Old if prev not null.
+                if (oldStart != null)
+                {
+                    rectangleX = oldStart.x * Consts.pixelsPerDimension + (Consts.wallThickness / 2) + (Consts.pictureBoxPaddingPixels / 2);
+                    rectangleY = oldStart.y * Consts.pixelsPerDimension + (Consts.wallThickness / 2) + (Consts.pictureBoxPaddingPixels / 2);
+                    bitmapGraphics.FillRectangle(bgColour, rectangleX, rectangleY, rectangleWidth, rectangleHeight);
+                }
+                // Add New.
+                rectangleX = newStart.x * Consts.pixelsPerDimension + (Consts.wallThickness / 2) + (Consts.pictureBoxPaddingPixels / 2);
+                rectangleY = newStart.y * Consts.pixelsPerDimension + (Consts.wallThickness / 2) + (Consts.pictureBoxPaddingPixels / 2);
+                bitmapGraphics.FillRectangle(startColour, rectangleX, rectangleY, rectangleWidth, rectangleHeight);
+            }
+            return mazeBitmap;
+        }
+        public static Bitmap changeFinishPoint(Bitmap mazeBitmap, Point oldStart, Point newStart, Brush bgColour)
+        {
+            using (Graphics bitmapGraphics = Graphics.FromImage(mazeBitmap))
+            {
+                int rectangleX;
+                int rectangleY;
+                int rectangleWidth = Consts.pixelsPerDimension - Consts.wallThickness;
+                int rectangleHeight = Consts.pixelsPerDimension - Consts.wallThickness;
+                // Remove Old if prev not null.
+                if (oldStart != null)
+                {
+                    rectangleX = oldStart.x * Consts.pixelsPerDimension + (Consts.wallThickness / 2) + (Consts.pictureBoxPaddingPixels / 2);
+                    rectangleY = oldStart.y * Consts.pixelsPerDimension + (Consts.wallThickness / 2) + (Consts.pictureBoxPaddingPixels / 2);
+                    bitmapGraphics.FillRectangle(bgColour, rectangleX, rectangleY, rectangleWidth, rectangleHeight);
+                }
+                // Add New.
+                rectangleX = newStart.x * Consts.pixelsPerDimension + (Consts.wallThickness / 2) + (Consts.pictureBoxPaddingPixels / 2);
+                rectangleY = newStart.y * Consts.pixelsPerDimension + (Consts.wallThickness / 2) + (Consts.pictureBoxPaddingPixels / 2);
+                bitmapGraphics.FillRectangle(finishColour, rectangleX, rectangleY, rectangleWidth, rectangleHeight);
+            }
+            return mazeBitmap;
+        }
+    }
+}
